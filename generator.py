4606118f73f721a7e7fc99d5b27d1ac6f3441fd5
AmrGalal7 <amrgalal7@yahoo.com>
2018-11-21 15:07:10 +0200

    added comments to the Generator Class

--- generator.py
+++ generator.py
@@ -1,21 +1,55 @@
 class Generator:
+    """
+    encodes a message based on a given polynomial/ divider
+
+    Class Attribute:
+    ----------------
+                    'GeneratorDivider': stores the polynomial, so it can be sent to the Verifier Class
+    """
 
     GeneratorDivider = None
 
     def __init__(self, message, divider):
-        self.message = message
+        """
+        Instances Attributes:
+        ---------------------
+                    'self.__message': holds the string of 0s and 1s of the message to be encoded
+                    'self.__divider': holds the string of 0s and 1s of the polynomial
+        """
+        self.__message = message
         self.__divider = divider
 
     def getDivider(self):
+        """
+        Return:
+        -------
+                    the string representation of the polynomial
+        """
         return self.__divider
 
     def encode(self):
-        messageDecimal = int(self.message, 2)
+        """
+        Return:
+        -------
+                    a string representation of the encoded message
+        """
+
+        # convert the string value of a binary number to int
+        messageDecimal = int(self.__message, 2)
         dividerDecimal = int(self.__divider, 2)
 
+        # calculate the remainder of the division of the message by the polynomial
         remainder = messageDecimal - (messageDecimal // dividerDecimal ) * dividerDecimal
 
+        # shift the binary message to the right x digits, where x is the degree of the polynomial, then subtract the remainder from it.
+        # shifting (for decimals): multiplication by 2 raised to some power
+        # degree of the polynomial: length of the polynomial
+        # the message becomes in the format: 0b10110
+        # the indexing to skip '0b'
+        # the message is then converted into string
         data = str(bin(messageDecimal * 2**len(self.__divider) - remainder))[2:]
+
+        # save the message on disk
         with open('transmitted_msg.txt', 'w') as f:
             f.write(data)
 
