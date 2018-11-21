ee5df46dfdb49b9c4a854f34dee469cfe51431b2
AmrGalal7 <amrgalal7@yahoo.com>
2018-11-21 15:07:27 +0200

    added comments to the Verifier Class

--- verifier.py
+++ verifier.py
@@ -1,13 +1,26 @@
 class Verifier:
+    """
+    checks if the received message is correct
+    """
 
     def __init__(self, data, divider):
-        self.data = data
-        self.divider = divider
+        """
+        Instances Attributes:
+        ---------------------
+                    'self.__message': holds the string of 0s and 1s of the message to be encoded
+                    'self.__divider': holds the string of 0s and 1s of the polynomial
+        """
+        self.__data = data
+        self.__divider = divider
 
     def verify(self):
-        dataDecimal = int(self.data, 2)
-        dividerDecimal = int(self.divider, 2)
+        """
+        prints 'message is correct' if the received message is correct, otherwise it prints 'message is not correct'
+        """
+        dataDecimal = int(self.__data, 2)
+        dividerDecimal = int(self.__divider, 2)
 
+        # if the message is divisible by the polynomial, then it's correct
         if dataDecimal % dividerDecimal == 0:
             print('message is correct')
         else:
