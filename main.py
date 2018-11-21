481221334d1b1e4e8bdb680f75175fbe1e8e8649
Ahmed Ebeid <a.ebeid@hotmail.com>
2018-11-21 20:00:17 +0200

    integrating generator, verifier and main

--- main.py
+++ main.py
@@ -1,46 +1,5 @@
-
-import re
-inputString= raw_input("Enter your command\n")
-print("text = ", inputString)
-counter =0
-for c in range(len(inputString)):
-	if inputString[c]=="|":
-		counter+=1
-#print(counter)
-
-def getDataFromFile(flag,inputString):
-#open file and get data from it
-	pureInput= inputString.replace(" ","")
-	if(flag ==1):
-		assignPosition = pureInput.find("<")
-		pureInput= pureInput[assignPosition+1:]
-	orPosition = pureInput.find("|")
-	fileName= pureInput[:orPosition]
-	if(flag==1):
-		inputString=pureInput[orPosition+1:]
-		file = open(fileName+".txt","r")
-		m=file.readline()
-		if(m.find("\n")):
-			m=m[:-2]
-		k=file.readline()
-		return m , k , inputString
-	if(flag==2):
-		bit=""
-		for t in range(len(inputString)):
-			if(inputString[t].isdigit()):
-				bit+=str(inputString[t])
-		return bit, inputString
-
-if(counter==1):
-	m , k , inputString = getDataFromFile(1,inputString)
-	print("m= ", m , " k= ", k , "remaining = ", inputString)
-if(counter==2):
-	m , k , inputString = getDataFromFile(1,inputString)
-	print("m= ", m , " k= ", k , "remaining = ", inputString)
-	bit , inputString = getDataFromFile(2,inputString)
-	print("Bit =",bit)
-
-
+from generator import Generator
+from verifier import Verifier
 
 def alter(convertedBit, message):
 
@@ -58,5 +17,54 @@ def alter(convertedBit, message):
 	return falseMassage;
 
 
-x = alter(3,"10111")
-print("X = ",x)
+inputString= input("Enter your command\n")
+counter =0
+for c in range(len(inputString)):
+	if inputString[c]=="|":
+		counter+=1
+
+def getDataFromFile(flag,inputString):
+#open file and get data from it
+	pureInput= inputString.replace(" ","")
+	if(flag ==1):
+		assignPosition = pureInput.find("<")
+		pureInput= pureInput[assignPosition+1:]
+	orPosition = pureInput.find("|")
+	fileName= pureInput[:orPosition]
+	if(flag==1):
+		inputString=pureInput[orPosition+1:]
+		file = open(fileName+".txt","r")
+		m=file.readline()
+		if(m.find("\n")):
+			m=m[:-1]
+		k=file.readline()
+		return m , k , inputString
+	if(flag==2):
+		bit=""
+		for t in range(len(inputString)):
+			if(inputString[t].isdigit()):
+				bit+=str(inputString[t])
+		return int(bit), inputString
+
+# case of generator and verifier
+if(counter==1):
+	m , k , inputString = getDataFromFile(1,inputString)
+	g = Generator(m, k)
+	genOutput = g.encode()
+	g.GeneratorDivider = g.getDivider()
+	v = Verifier(genOutput, g.GeneratorDivider)
+	v.verify()
+# case of generator, alter, and verifier
+if(counter==2):
+	m , k , inputString = getDataFromFile(1,inputString)
+	g = Generator(m, k)
+	genOutput = g.encode()
+	g.GeneratorDivider = g.getDivider()
+	v = Verifier(genOutput, g.GeneratorDivider)
+	v.verify()
+
+	bit , inputString = getDataFromFile(2,inputString)
+	messageAltered = alter(bit, genOutput)
+	v = Verifier(messageAltered, g.GeneratorDivider)
+	v.verify()
+
